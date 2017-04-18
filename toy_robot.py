9b015c884a53eea9c28c347ad9b3756d682add55
Marino Frana <marino.frana@emovis.hr>
2017-04-18 17:42:55 +0200

    * added enumeration Direction * added class ToyRobot (partial implementation)

---
+++ toy_robot.py
@@ -0,0 +1,32 @@
+import enum
+
+
+class Direction(enum.Enum):
+	NORTH = 1
+	SOUTH = 2
+	EAST = 3
+	WEST = 4
+
+
+class ToyRobot:
+	_position_x = None
+	_position_y = None
+	_direction = None
+
+	def __init__(self):
+		pass
+
+	def place(x, y, f):
+		pass
+
+	def move(self):
+		pass
+
+	def left(self):
+		pass
+
+	def right(self):
+		pass
+
+	def report(self):
+		pass

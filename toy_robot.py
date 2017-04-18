966277554c3e9eef9d4dbf55d83a6bad8060cdb5
Marino Frana <marino.frana@emovis.hr>
2017-04-18 17:49:28 +0200

    * implemented requirement to ignore input if robot is not placed

--- toy_robot.py
+++ toy_robot.py
@@ -16,17 +16,42 @@ class ToyRobot:
 	def __init__(self):
 		pass
 
+	@property
+	def _is_placed(self):
+		if self._position_x is None:
+			return False
+
+		if self._position_y is None:
+			return False
+
+		if self._direction is None:
+			return False
+
+		return True
+
 	def place(x, y, f):
 		pass
 
 	def move(self):
+		if self._is_placed is False:
+			return
+
 		pass
 
 	def left(self):
+		if self._is_placed is False:
+			return
+
 		pass
 
 	def right(self):
+		if self._is_placed is False:
+			return
+
 		pass
 
 	def report(self):
+		if self._is_placed is False:
+			return
+
 		pass
